95ed89cc0c3af2ee27789fc8bb5514f923624cc4
Jen Gilbert <jgilbert@lyft.com>
2018-04-28 20:52:59 -0700

    Add starter project boilerplate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React, { Component } from 'react';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList contacts={[
-          {name: 'Tyler'},
-          {name: 'Karen'},
-          {name: 'Richard'}
-        ]} />
-        <ContactList contacts={[
-          {name: 'Bob'},
-          {name: 'Steve'},
-          {name: 'Harvey'}
-        ]} />
       </div>
     );
   }
