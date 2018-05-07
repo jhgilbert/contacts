fc6cfb9f20ebb5aff9993e9931f299c7ade885f8
Jen Gilbert <jgilbert@lyft.com>
2018-05-07 01:52:40 -0500

    Validate stuff

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function ListContacts(props) {
 
@@ -22,7 +23,12 @@ function ListContacts(props) {
         </li>
       ))}
     </ol>
-  );
+  )
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts;
